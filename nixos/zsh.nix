bc1e1e2bfb74d6756b5c54cdb8cb7420d170fcc0
maybemellomike <ubuntu9786@gmail.com>
2026-02-13 09:45:30 -0500

    zsh alias add

--- nixos/zsh.nix
+++ nixos/zsh.nix
@@ -6,6 +6,7 @@
     enable = true;
     shellAliases = {
           conf = "du -a ~/.config/* | awk '{print $2}' | fzf --preview 'less {}' | xargs -r $EDITOR";
+          nixconf = "du -a /etc/nixos/* | awk '{print $2}' | fzf --preview 'less {}' | xargs -r doas $EDITOR";
           dox = "du -a ~/Documents/* | awk '{print $2}' | fzf --preview 'less {}' | xargs -r $EDITOR";
           tses = "session=$(tmux list-sessions -F '#S' | fzf) || exit tmux attach -t '$session' ";
           config = "doas vim /etc/nixos/configuration.nix";
